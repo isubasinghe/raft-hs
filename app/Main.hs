e57e3cee4ba6caa5c546af7934d7081ff5836800
isubasinghe <isubasinghe@student.unimelb.edu.au>
2021-07-03 02:22:59 +1000

    feat: added database bindings

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,18 @@
 module Main where
-import Options.Applicative
-import CLI
-import API
 
+import API
+import CLI
+import Options.Applicative
 
 main :: IO ()
 main = do
-    options <- execParser opts
-    runApp 
-    where
-        opts = info (cliParser <**> helper)
-                ( fullDesc 
-                  <> progDesc "A simple key value store in Haskell implemented via acid state with fail over implemented via Raft"
-                  <> header "RaftKVS")
-
+  -- options <- execParser opts
+  runApp
+  where
+    opts =
+      info
+        (cliParser <**> helper)
+        ( fullDesc
+            <> progDesc "A simple key value store in Haskell implemented via acid state with fail over implemented via Raft"
+            <> header "RaftKVS"
+        )
